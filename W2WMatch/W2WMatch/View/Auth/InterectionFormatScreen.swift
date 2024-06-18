3f653f317e58d325f5ced7fb01d5cb6be40bb263
Igor Krysin <igor_krisin@mail.ru>
2024-06-18 22:37:27 +0300

    intersection format screen is done

---
+++ W2WMatch/W2WMatch/View/Auth/InterectionFormatScreen.swift
@@ -0,0 +1,75 @@
+//
+//  InterectionFormatScreen.swift
+//  W2WMatch
+//
+//  Created by Игорь Крысин on 17.06.2024.
+//
+
+import SwiftUI
+
+struct InterectionFormatScreen: View {
+    
+    let options = ["Не определилась с форматом, но готова к обсуждению", "Совместный прямой эфир", "Совместный прямой reels", "Совместный прямой пост", "Выпустить совместный продукт", "Провести совместно мероприятие"]
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                Text("Очень хорошо! Осталось всего пару вопросов")
+                    .foregroundColor(Color("W2wBlueColor"))
+                    .font(.custom("PoiretOne-Regular", size: 34))
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 50)
+                    .frame(width: geometry.size.width - 80)
+                    .frame(maxWidth: .infinity)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .padding(.bottom, 18)
+                
+                Text("Какие форматы взаимодействия вас интересуют?")
+                    .font(Font.custom("Manrope", size: 14))
+                    .tracking(0.28)
+                    .foregroundColor(Color("W2wBlueColor"))
+                    .padding(.bottom, 8)
+                    .multilineTextAlignment(.leading)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .fixedSize(horizontal: false, vertical: true)
+                Text("Выберите один или несколько вариантов")
+                    .font(Font.custom("Manrope", size: 12))
+                    .tracking(0.28)
+                    .foregroundColor(Color("SecondaryText"))
+                    .padding(.bottom, 24)
+                    .multilineTextAlignment(.leading)
+                
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                ForEach(options, id: \.self) { option in
+                    TableRow(text: option)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                Button("Далее") {
+                    //отправка данных на сервер
+                }
+                .frame(width: geometry.size.width - 120, height: 45.0)
+                .foregroundStyle(.white)
+                .background {
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color("W2wLightBlueColor"))
+                }
+                .padding(.top)
+                Image("Vector")
+                    .padding(.top, 20)
+            }
+            
+            .frame(width: geometry.size.width - 120, height: geometry.size.height)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+        Spacer()
+        
+        
+    }
+}
+
+
+#Preview {
+    InterectionFormatScreen()
+}
+
+
---
+++ W2WMatch/W2WMatch/View/TableRow.swift
@@ -0,0 +1,60 @@
+//
+//  TableRow.swift
+//  W2WMatch
+//
+//  Created by Игорь Крысин on 17.06.2024.
+//
+
+import SwiftUI
+
+struct TableRow: View {
+    @State private var isChecked: Bool = false
+    var text: String
+    
+    var body: some View {
+        HStack {
+            Toggle("", isOn: $isChecked)
+                .toggleStyle(CheckBoxToggelStyle())
+                .padding(.leading, 0)
+            
+                Text(text)
+                    .foregroundColor(Color("W2wBlueColor"))
+                    .padding(.leading, 0)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .font(Font.custom("Manrope", size: 14))
+            
+            Spacer()
+            
+           
+        }
+        .padding(.trailing, 0)
+        .frame(maxWidth: .infinity)
+        .padding()
+        .background(
+        RoundedRectangle(cornerRadius: 10)
+            .fill(Color.white)
+            .shadow(color: Color.gray.opacity(0.2), radius: 10, x: 0, y: 2)
+            
+        )
+    }
+}
+
+struct CheckBoxToggelStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        Button(action: {
+            configuration.isOn.toggle()
+        }) {
+            HStack {
+                Image(systemName: configuration.isOn ? "checkmark.square" : "square")
+                    .foregroundColor(configuration.isOn ? Color("FrameRegistration") : Color("FrameRegistration"))
+                    .padding(.leading, 5)
+                configuration.label
+            }
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+}
+
+#Preview {
+    TableRow(text: "ddddd")
+}
