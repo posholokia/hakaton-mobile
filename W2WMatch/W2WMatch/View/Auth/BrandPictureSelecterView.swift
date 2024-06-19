b840bc526c5ae9b2d045b1e7d96b3ec7f6b04a99
Igor Krysin <igor_krisin@mail.ru>
2024-06-19 20:38:14 +0300

    before merge conflict

--- W2WMatch/W2WMatch/View/Auth/BrandPictureSelecterView.swift
+++ W2WMatch/W2WMatch/View/Auth/BrandPictureSelecterView.swift
@@ -2,51 +2,17 @@
 //  BrandPictureSelecterView.swift
 //  W2WMatch
 //
-//  Created by Floron on 19.06.2024.
+//  Created by Игорь Крысин on 19.06.2024.
 //
 
 import SwiftUI
-import PhotosUI
 
 struct BrandPictureSelecterView: View {
-    @State private var photosPickerItem: PhotosPickerItem?
-    let filter: PHPickerFilter = .not(.videos)
-    
     var body: some View {
-        VStack {
-                   PhotosPicker(selection: $photosPickerItem, matching: filter) {
-                     Label("Select Photo", systemImage: "photo")
-                   }
-               }
-               .padding()
-        
-        Button("Done") {
-            let img = UIImage()
-            let base64 = img.base64
-            let rebornImg = base64?.imageFromBase64
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
- 
 
 #Preview {
     BrandPictureSelecterView()
-}
-
-
-
-
-extension UIImage {
-    var base64: String? {
-        self.jpegData(compressionQuality: 1)?.base64EncodedString()
-    }
-}
-
-extension String {
-    var imageFromBase64: UIImage? {
-        guard let imageData = Data(base64Encoded: self, options: .ignoreUnknownCharacters) else {
-            return nil
-        }
-        return UIImage(data: imageData)
-    }
 }
